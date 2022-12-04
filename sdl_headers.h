438e9503c2ca6f42700fdb3aeee98181836335be
Lizzy Chu <b08608021@ntu.edu.tw>
2022-12-04 12:06:43 +0800

    add sdl_headers.h

---
+++ sdl_headers.h
@@ -0,0 +1,14 @@
+#ifndef __MINE_SDL_HEADERS_H__
+#define __MINE_SDL_HEADERS_H__
+
+#ifdef __APPLE__
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#endif
+
+#ifdef __MINGW32__
+#include <SDL.h>
+#include <SDL_image.h>
+#endif
+
+#endif
